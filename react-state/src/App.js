d86bd4e21faf6f00cebe660968ae367586441c7c
Taharka2000 <sammbasow1999@gmail.com>
2023-09-22 17:37:49 +0000

    react-state

---
+++ react-state/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "Ousmane Sonko",
+        bio: "Né le 15 juillet 1974 à Thiès, est un homme politique sénégalais, député à l'Assemblée nationale de 2017 à 2022 et maire de la ville de Ziguinchor depuis 2022. Il est le président du parti Patriotes africains du Sénégal pour le travail, l'éthique et la fraternité jusqu'à sa dissolution en juillet 2023",
+        image: "https://gmetech.info/wp-content/uploads/2020/07/Ousmane-Sonko-slide.png",
+        profession: "vérificateur fiscal et chef de Brigade de vérification fiscale"
+      },
+      show: false,
+      interval: 0,
+    };
+  }
+  toglee = () => {
+    this.setState((prevent) => ({
+      show: !prevent.show
+    }))
+  }
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState((prevent) => ({
+        interval: prevent.interval + 1,
+      }));
+    }, 1000); // Mettre à jour l'intervalle toutes les 1000 ms (1 seconde)
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  render() {
+    const { fullName, bio, image, profession } = this.state.person;
+    return (
+      <>
+       <div className="flex justify-center">
+                  <img className="w-[10%] " src={image} alt="" />
+                </div>
+        <div className="flex flex-col items-center">
+          {
+            this.state.show && (
+              <div className="flex flex-col text-center w-[25%] ">
+                <div className="shadow-2xl">
+                  <h1 className="text-lg font-bold">Nom:{fullName}</h1>
+                  <h2 className="font-medium">{bio}</h2>
+                  <h3 className="text-lg font-bold">Profession:{profession}</h3>
+                </div>
+              </div>
+            )
+          }
+          <div className="text-center">
+            <button className="ml-[18px]  middle none center rounded-lg bg-emerald-900 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              data-ripple-light="true" onClick={this.toglee}>
+              {
+                this.state.show ? "Hidden" : "Show Me"
+              }
+            </button>
+          </div>
+        </div>
+        <p>Time Interval Since Mount: {this.state.interval} seconds</p>
+      </>
+    )
+  }
+}
+export default App;
